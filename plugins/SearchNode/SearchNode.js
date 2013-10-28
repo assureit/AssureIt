f8ef8ecf8a0dffde573b3739b8d7a2b02007b0c1
Shunsuke Shida <shunsuke.sida@gmail.com>
2013-10-28 17:19:01 +0900

    Remove SearchNodeMenu plugin

--- plugins/SearchNode/SearchNode.js
+++ plugins/SearchNode/SearchNode.js
@@ -8,40 +8,6 @@ var SearchNodePlugIn = (function (_super) {
     __extends(SearchNodePlugIn, _super);
     function SearchNodePlugIn(plugInManager) {
         _super.call(this, plugInManager);
-
-        this.MenuBarContentsPlugIn = new SearchNodeMenuPlugIn(plugInManager);
     }
     return SearchNodePlugIn;
 })(AssureIt.PlugInSet);
-
-var SearchNodeMenuPlugIn = (function (_super) {
-    __extends(SearchNodeMenuPlugIn, _super);
-    function SearchNodeMenuPlugIn(plugInManager) {
-        _super.call(this, plugInManager);
-        this.element = null;
-        this.caseViewer = null;
-    }
-    SearchNodeMenuPlugIn.prototype.IsEnabled = function (caseViewer, caseModel) {
-        return true;
-    };
-
-    SearchNodeMenuPlugIn.prototype.Delegate = function (caseViewer, caseModel, element, serverApi) {
-        var _this = this;
-        this.caseViewer = caseViewer;
-        this.element = element;
-        element.append('<a href="#" ><img id="center" src="' + serverApi.basepath + 'images/scale.png" title="Search" alt="Search" /></a>');
-        $('#center').unbind('click');
-        $('#center').click(function () {
-            _this.Center();
-        });
-
-        return true;
-    };
-
-    SearchNodeMenuPlugIn.prototype.Center = function () {
-        var thisLabel = this.element.children('h4').text();
-        var thisNodeView = this.caseViewer.ViewMap[thisLabel];
-        return;
-    };
-    return SearchNodeMenuPlugIn;
-})(AssureIt.MenuBarContentsPlugIn);
