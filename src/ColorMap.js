f9082f3f70241a7ce08252c0ade4c2d935486795
Okamoto Yuki <yuki@Okamotos-MacBook-Pro.local>
2013-10-28 20:56:54 +0900

    add ColorMap.js

---
+++ src/ColorMap.js
@@ -0,0 +1,8 @@
+var AssureIt;
+(function (AssureIt) {
+    AssureIt.Color = {
+        Default: "assureit-default",
+        HighLight: "assureit-highlight",
+        Danger: "assureit-danger"
+    };
+})(AssureIt || (AssureIt = {}));
